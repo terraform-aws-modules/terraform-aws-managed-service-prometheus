f26527ac36569120b8f76dedd0dea645c8318a66
Bryant Biggs <bryantbiggs@gmail.com>
2022-07-28 11:25:29 -0400

    feat: Add support for using an existing/external workspace (#6)

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
+locals {
+  workspace_id = var.create_workspace ? aws_prometheus_workspace.this[0].id : var.workspace_id
+}
+
 ################################################################################
 # Workspace
 ################################################################################
 
 resource "aws_prometheus_workspace" "this" {
-  count = var.create ? 1 : 0
+  count = var.create && var.create_workspace ? 1 : 0
 
   alias = var.workspace_alias
   tags  = var.tags
@@ -16,7 +20,7 @@ resource "aws_prometheus_workspace" "this" {
 resource "aws_prometheus_alert_manager_definition" "this" {
   count = var.create ? 1 : 0
 
-  workspace_id = aws_prometheus_workspace.this[0].id
+  workspace_id = local.workspace_id
   definition   = var.alert_manager_definition
 }
 
@@ -28,6 +32,6 @@ resource "aws_prometheus_rule_group_namespace" "this" {
   for_each = var.create ? var.rule_group_namespaces : {}
 
   name         = each.value.name
-  workspace_id = aws_prometheus_workspace.this[0].id
+  workspace_id = local.workspace_id
   data         = each.value.data
 }
